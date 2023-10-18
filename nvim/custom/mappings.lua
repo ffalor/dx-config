6533b552b04d7ce7286eee429c2361319be56a33
ffalor <35144141+ffalor@users.noreply.github.com>
2023-10-18 14:59:16 -0500

    add lazygit plugin

--- nvim/custom/mappings.lua
+++ nvim/custom/mappings.lua
@@ -2,30 +2,41 @@
 local M = {}
 
 M.general = {
-  n = {
-    [";"] = { ":", "enter command mode", opts = { nowait = true } },
-    ["J"] = { "mzJ`z", "Move line below to current line" },
-    ["<C-d>"] = { "<C-d>zz", "Jump half page down and center" },
-    ["<C-u"] = { "<C-u>zz", "Jump half page up and center" },
-    ["n"] = { "nzzzv", "Next search term" },
-    ["N"] = { "Nzzzv", "Previous search term" },
-    ["<leader>rw"] = { ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>", "Replace a word everywhere" },
-    ["gh"] = {"<cmd>lua vim.lsp.buf.hover()<CR>", "Show LSP definition hover."}
-  },
-  v = {
-    ["<leader>y"] = { '"+y', "Yank to system register" },
-    ["J"] = { ":m '>+1<CR>gv=gv", "Move block down and indent" },
-    ["K"] = { ":m '<-2<CR>gv=gv", "Move block up and indent" },
-  },
-  x = {
-    ["<leader>p"] = { '"_dP', "Replace paste without copy to registery" },
-  },
+    n = {
+        [";"] = {
+            ":",
+            "enter command mode",
+            opts = {
+                nowait = true
+            }
+        },
+        ["J"] = {"mzJ`z", "Move line below to current line"},
+        ["<C-d>"] = {"<C-d>zz", "Jump half page down and center"},
+        ["<C-u"] = {"<C-u>zz", "Jump half page up and center"},
+        ["n"] = {"nzzzv", "Next search term"},
+        ["N"] = {"Nzzzv", "Previous search term"},
+        ["<leader>rw"] = {":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>", "Replace a word everywhere"}
+    },
+    v = {
+        ["<leader>y"] = {'"+y', "Yank to system register"},
+        ["J"] = {":m '>+1<CR>gv=gv", "Move block down and indent"},
+        ["K"] = {":m '<-2<CR>gv=gv", "Move block up and indent"}
+    },
+    x = {
+        ["<leader>p"] = {'"_dP', "Replace paste without copy to registery"}
+    }
 }
 
 M.telescope = {
- n = {
-    ["<leader>fg"] = { "<cmd> Telescope git_files <CR>", "Find project files" },
-  }
+    n = {
+        ["<leader>fg"] = {"<cmd> Telescope git_files <CR>", "Find project files"}
+    }
+}
+
+M.lazygit = {
+    n = {
+        ["<leader>lg"] = {"<cmd> LazyGit <CR>", "Open lazygit"}
+    }
 }
 
 return M
