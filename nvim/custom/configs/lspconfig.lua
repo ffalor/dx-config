d90d800b43b26f64fd9517e1e88f59c192516b9a
Frank Falor <35144141+ffalor@users.noreply.github.com>
2023-12-05 21:27:20 -0600

    add noice plugin

--- nvim/custom/configs/lspconfig.lua
+++ nvim/custom/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 local util = require "lspconfig/util"
 -- if you just want default config for the servers then put them in a table
--- local servers = {"gopls"}
+-- local servers = {"lua-language-server"}
 --
 -- for _, lsp in ipairs(servers) do
 --   lspconfig[lsp].setup {
@@ -12,9 +12,6 @@ local util = require "lspconfig/util"
 --     capabilities = capabilities,
 --   }
 -- end
---
---
--- lspconfig.pyright.setup { blabla}
 
 lspconfig.gopls.setup {
   on_attach = on_attach,
@@ -37,4 +34,35 @@ lspconfig.pyright.setup {
   on_attach = on_attach,
   capabilities = capabilities,
   filetypes = { "python" },
+}
+
+lspconfig.lua_ls.setup {
+  on_init = function(client)
+    local path = client.workspace_folders[1].name
+    if not vim.loop.fs_stat(path..'/.luarc.json') and not vim.loop.fs_stat(path..'/.luarc.jsonc') then
+      client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
+        Lua = {
+          runtime = {
+            -- Tell the language server which version of Lua you're using
+            -- (most likely LuaJIT in the case of Neovim)
+            version = 'LuaJIT'
+          },
+          -- Make the server aware of Neovim runtime files
+          workspace = {
+            checkThirdParty = false,
+            library = {
+              vim.env.VIMRUNTIME
+              -- "${3rd}/luv/library"
+              -- "${3rd}/busted/library",
+            }
+            -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+            -- library = vim.api.nvim_get_runtime_file("", true)
+          }
+        }
+      })
+
+      client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+    end
+    return true
+  end
 }
